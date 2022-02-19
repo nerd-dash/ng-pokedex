c0611dc0cca03f412dbf2ecb2dbaac7550930d02
Flávio Barcelos <arantesbarcelos@gmail.com>
2022-02-19 01:03:24 +0000

    refactor(pokegame.component): create sub routes to pokegame

--- src/app/pokegame/pokegame-routing.module.ts
+++ src/app/pokegame/pokegame-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokegameComponent } from './container/pokegame/pokegame.component';
+import { RandomPokemonComponent } from './container/random-pokemon/random-pokemon.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PokegameComponent,
+    children: [
+      { path: '', component: RandomPokemonComponent },
+      {
+        path: 'pokedex',
+        loadChildren: () =>
+          import('../pokedex/pokedex.module').then(
+            (module) => module.PokedexModule
+          ),
+      },
+    ],
   },
 ];
 
