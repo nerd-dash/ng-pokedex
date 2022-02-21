1a34c4f8a1d6af8b9dd9ceb689e4854950055ff2
Flávio Barcelos <arantesbarcelos@gmail.com>
2022-02-21 22:59:09 +0000

    refactor(poke-auth): updates to use publishable-service

--- src/app/services/poke-auth.service.spec.ts
+++ src/app/services/poke-auth.service.spec.ts
@@ -2,24 +2,43 @@ import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { AccessToken } from '../models/AccessToken';
+import { PublishableService } from '../models/PublishableService';
 import { User } from '../models/User';
+import { PUBLISHABLE_SERVICE } from '../tokens/publishable-service-token';
 import { PokeAuthService } from './poke-auth.service';
 
 describe('PokeAuthService', () => {
   let service: PokeAuthService;
   let httpTestingController: HttpTestingController;
+  let publishiableServiceSpy: jasmine.SpyObj<PublishableService<User>>;
+
+  const user: User = {
+    id: 1,
+    email: 'email',
+    password: 'password',
+  };
 
   const accessToken: AccessToken<User> = {
     accessToken: 'accessToken',
+    payload: user,
   };
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    publishiableServiceSpy = jasmine.createSpyObj<PublishableService<User>>({
+      asObservable$: of(user),
+      next: undefined,
+    });
+
     TestBed.configureTestingModule({
+      providers: [
+        PokeAuthService,
+        { provide: PUBLISHABLE_SERVICE, useValue: publishiableServiceSpy },
+      ],
       imports: [HttpClientTestingModule],
-      providers: [PokeAuthService],
     });
     service = TestBed.inject(PokeAuthService);
     httpTestingController = TestBed.inject(HttpTestingController);
@@ -53,6 +72,10 @@ describe('PokeAuthService', () => {
         .login$({ email: 'email', password: 'password' })
         .subscribe((_accessToken) => {
           expect(_accessToken).toBe(accessToken);
+          expect(_accessToken.payload).not.toBeUndefined();
+          expect(publishiableServiceSpy.next).toHaveBeenCalledOnceWith(
+            _accessToken.payload
+          );
         });
 
       const req = httpTestingController.expectOne(
@@ -62,6 +85,14 @@ describe('PokeAuthService', () => {
       expect(req.request.method).toEqual('POST');
 
       req.flush(accessToken);
+    });
+  });
+
+  describe('loggedInd$', () => {
+    it('should return data from the current logged in user', () => {
+      service.loggedIn$().subscribe(() => {
+        expect(publishiableServiceSpy.asObservable$).toHaveBeenCalledTimes(1);
+      });
     });
   });
 });
--- src/app/services/poke-auth.service.ts
+++ src/app/services/poke-auth.service.ts
@@ -1,24 +1,33 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { AccessToken } from '../models/AccessToken';
 import { AuthService } from '../models/AuthService';
+import { PublishableService } from '../models/PublishableService';
 import { User } from '../models/User';
+import { PUBLISHABLE_SERVICE } from '../tokens/publishable-service-token';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PokeAuthService implements AuthService<AccessToken<User>> {
-  constructor(private httpClient: HttpClient) {}
+export class PokeAuthService implements AuthService<User, AccessToken<User>> {
+  constructor(
+    @Inject(PUBLISHABLE_SERVICE) private publishable: PublishableService<User>,
+    private httpClient: HttpClient
+  ) {}
   login$ = (loginData: Partial<User>) =>
-    this.httpClient.post<AccessToken<User>>(
-      `${environment.SERVER_BASE_URL}/login`,
-      { ...loginData }
-    );
+    this.httpClient
+      .post<AccessToken<User>>(`${environment.SERVER_BASE_URL}/login`, {
+        ...loginData,
+      })
+      .pipe(tap((data) => this.publishable.next(data.payload)));
 
   register$ = (data: Partial<User>) =>
     this.httpClient.post<AccessToken<User>>(
       `${environment.SERVER_BASE_URL}/register`,
       { ...data }
     );
+
+  loggedIn$ = () => this.publishable.asObservable$();
 }
