ddb7286791cc78502a6646fd1d38ac2f1a8ce4e7
Flávio Barcelos <arantesbarcelos@gmail.com>
2022-03-08 00:17:32 +0000

    remove unsued files

--- src/app/tokens/user-auth-service.token.ts
+++
@@ -1,7 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { AuthService } from '../models/AuthService';
-import { User } from '../models/User';
-
-export const USER_AUTH_SERVICE = new InjectionToken<AuthService<User>>(
-  'user-auth-service'
-);
