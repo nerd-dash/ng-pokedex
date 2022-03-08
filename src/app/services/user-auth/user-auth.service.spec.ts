8d82110eb11d2837a7008e91c32b60ce0e024bb9
Flávio Barcelos <arantesbarcelos@gmail.com>
2022-03-08 00:37:24 +0000

    refactor(user auth ): delegate state to user state

--- src/app/services/user-auth/user-auth.service.spec.ts
+++ src/app/services/user-auth/user-auth.service.spec.ts
@@ -3,21 +3,17 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
-import { StorageService } from 'src/app/models/StorageService';
-import { STORAGE_SERVICE } from 'src/app/tokens/user-storage-service.token';
-import { environment } from 'src/environments/environment';
+import { AuthStateService } from 'src/app/models/AuthStateService';
+import { USER_STATE_SERVICE } from 'src/app/tokens/game-state/user-state-service.token';
 import { AccessToken } from '../../models/AccessToken';
-import { PublishableService } from '../../models/PublishableService';
 import { User } from '../../models/User';
-import { PUBLISHABLE_SERVICE } from '../../tokens/publishable-service.token';
+import { USER_AUTH_ENDPOINTS } from './user-auth.endpoints';
 import { UserAuthService } from './user-auth.service';
 
 describe('UserAuthService', () => {
   let service: UserAuthService;
   let httpTestingController: HttpTestingController;
-  let publishiableServiceSpy: jasmine.SpyObj<PublishableService<User>>;
-  let storageServiceSpy: jasmine.SpyObj<StorageService<AccessToken<User>>>;
+  let authStateServiceSpy: jasmine.SpyObj<AuthStateService<User>>;
 
   const user: User = {
     id: 1,
@@ -31,22 +27,14 @@ describe('UserAuthService', () => {
   };
 
   beforeEach(async () => {
-    publishiableServiceSpy = jasmine.createSpyObj<PublishableService<User>>({
-      asObservable$: of(user),
-      next: undefined,
+    authStateServiceSpy = jasmine.createSpyObj<AuthStateService<User>>({
+      updateToken: undefined,
     });
-
-    storageServiceSpy = jasmine.createSpyObj<StorageService<AccessToken<User>>>(
-      {
-        setItem: undefined,
-      }
-    );
 
     TestBed.configureTestingModule({
       providers: [
         UserAuthService,
-        { provide: PUBLISHABLE_SERVICE, useValue: publishiableServiceSpy },
-        { provide: STORAGE_SERVICE, useValue: storageServiceSpy },
+        { provide: USER_STATE_SERVICE, useValue: authStateServiceSpy },
       ],
       imports: [HttpClientTestingModule],
     });
@@ -66,9 +54,7 @@ describe('UserAuthService', () => {
           expect(_accessToken).toBe(accessToken);
         });
 
-      const req = httpTestingController.expectOne(
-        `${environment.SERVER_BASE_URL}/register`
-      );
+      const req = httpTestingController.expectOne(USER_AUTH_ENDPOINTS.Register);
 
       expect(req.request.method).toEqual('POST');
 
@@ -83,44 +69,29 @@ describe('UserAuthService', () => {
         .subscribe((_accessToken) => {
           expect(_accessToken).toBe(accessToken);
           expect(_accessToken.payload).not.toBeUndefined();
-          expect(publishiableServiceSpy.next).toHaveBeenCalledOnceWith(
-            _accessToken.payload
-          );
         });
 
-      const req = httpTestingController.expectOne(
-        `${environment.SERVER_BASE_URL}/login`
-      );
+      const req = httpTestingController.expectOne(USER_AUTH_ENDPOINTS.Login);
 
       expect(req.request.method).toEqual('POST');
 
       req.flush(accessToken);
     });
 
-    it('should create an iten on local storage called token with the accessToken', () => {
+    it('should get the login info and send it to the state service', () => {
       service
         .login$({ email: 'email', password: 'password' })
         .subscribe((_accessToken) => {
-          expect(storageServiceSpy.setItem).toHaveBeenCalledOnceWith(
+          expect(authStateServiceSpy.updateToken).toHaveBeenCalledOnceWith(
             _accessToken
           );
         });
 
-      const req = httpTestingController.expectOne(
-        `${environment.SERVER_BASE_URL}/login`
-      );
+      const req = httpTestingController.expectOne(USER_AUTH_ENDPOINTS.Login);
 
       expect(req.request.method).toEqual('POST');
 
       req.flush(accessToken);
-    });
-  });
-
-  describe('loggedInd$', () => {
-    it('should return data from the current logged in user', () => {
-      service.loggedIn$().subscribe(() => {
-        expect(publishiableServiceSpy.asObservable$).toHaveBeenCalledTimes(1);
-      });
     });
   });
 });
