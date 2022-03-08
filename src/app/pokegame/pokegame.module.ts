ff732d912353def31d51878343bc82230bc589da
Flávio Barcelos <arantesbarcelos@gmail.com>
2022-03-08 00:38:23 +0000

    refactor(various): changes on the injections

--- src/app/pokegame/pokegame.module.ts
+++ src/app/pokegame/pokegame.module.ts
@@ -3,10 +3,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PokeCardModule } from '../pokedex/component/poke-card.module';
-import { PokemonGameStateService } from '../services/pokemon-game-state/pokemon-game-state.service';
-import { UserAuthService } from '../services/user-auth/user-auth.service';
-import { POKEMON_GAME_STATE_SERVICE } from '../tokens/game-state/pokemon-game-state-service.token';
-import { USER_AUTH_SERVICE } from '../tokens/user-auth-service.token';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { PokegameComponent } from './container/pokegame/pokegame.component';
 import { RandomPokemonComponent } from './container/random-pokemon/random-pokemon.component';
@@ -26,16 +22,6 @@ import { PokegameRoutingModule } from './pokegame-routing.module';
     WhosThatPokemonComponent,
     RandomPokemonComponent,
     NavbarComponent,
-  ],
-  providers: [
-    {
-      provide: USER_AUTH_SERVICE,
-      useExisting: UserAuthService,
-    },
-    {
-      provide: POKEMON_GAME_STATE_SERVICE,
-      useExisting: PokemonGameStateService,
-    },
   ],
 })
 export class PokegameModule {}
